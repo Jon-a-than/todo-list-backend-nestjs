f21e65e6e27bac88596644501d18f2ec5cae4069
Jon-a-than <2140145186@qq.com>
2023-03-22 16:22:24 +0800

    🔧 配置@路径别名

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UserModule } from './user/user.module'
-import { TodoModule } from './todo/todo.module'
-import { AuthModule } from './auth/auth.module'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { UserModule } from '@/user/user.module'
+import { TodoModule } from '@/todo/todo.module'
+import { AuthModule } from '@/auth/auth.module'
 
 @Module({
   imports: [
