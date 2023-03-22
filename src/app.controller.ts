6110e20d62b2bb27a52063050dd0f3c963b555e9
Jon-a-than <2140145186@qq.com>
2023-03-22 21:41:14 +0800

    🐛 更新路径

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { AppService } from '@/app.service'
 import { AuthService } from './auth/auth.service'
-import { JwtAuthGuard } from './auth/jwt-auth.guard'
-import { LocalAuthGuard } from './auth/local-auth.guard'
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard'
+import { LocalAuthGuard } from './auth/guards/local-auth.guard'
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common'
 
 import type { UserDBInfo } from '@/user/types/user.type'
