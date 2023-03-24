a36b027c0f789e1a9cff01fba639d0bbc7736071
Jon-a-than <2140145186@qq.com>
2023-03-24 12:42:35 +0800

    🐛 修复jest路径别名

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  rootDir: './src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  coverageDirectory: '../coverage',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+}
+
+export default config
